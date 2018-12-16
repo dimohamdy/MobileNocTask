89ded6385800643b74e5b0d933a9353b04d6ad87
Amaal <amaal.kadry@gmail.com>
2018-12-16 20:06:32 +0200

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Amaal on 12/16/18.
+//  Copyright © 2018 Amaal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
